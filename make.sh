751602231e3e2b5eb3294dec636b91c8532c266c
Roy O'Young <roy2220@outlook.com>
2021-11-09 00:07:05 +0800

    Fix make.sh

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ DEBUG=${DEBUG:+1}
 NDEBUG=$([ -n "${DEBUG}" ] || echo 1)
 XPWD=${XPWD:-${PWD}}
 
-docker run --user="$(id -u):$(id -g)" --rm --interactive --tty \
+docker run --user="$(id -u):$(id -g)" --rm --interactive $([ ! -t 1 ] || echo --tty) \
 	--volume=/var/run/docker.sock:/var/run/docker.sock \
 	--volume="${XPWD}:/workspace" \
 	--workdir=/workspace \
