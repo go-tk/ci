169a23a3466ffccd7523f8140dcdc25b022b5e87
Roy O'Young <roy2220@outlook.com>
2022-01-30 18:50:02 +0800

    Enhance make.sh

--- make.sh
+++ make.sh
@@ -2,14 +2,26 @@ set -eu${DEBUG:+x}
 
 DEBUG=${DEBUG:+1}
 NDEBUG=$([ -n "${DEBUG}" ] || echo 1)
-if [ -t 1 ]; then TTY=1; else TTY=; fi
-XPWD=${XPWD:-${PWD}}
 
-docker run --user="$(id -u):$(id -g)" --rm --interactive ${TTY:+--tty} \
-	--volume=/var/run/docker.sock:/var/run/docker.sock \
-	--volume="${XPWD}:${PWD}" \
-	--workdir="${PWD}" \
-	--env=DEBUG=${DEBUG} \
-	--env=NDEBUG=${NDEBUG} \
-	--env="XPWD=${XPWD}" \
-	ghcr.io/go-tk/ci:v1.3.1 make ${DEBUG:+--trace} "${@}"
+COMMAND='docker run --user="$(id -u):$(id -g)" --rm --interactive'
+
+if [ -t 1 ]; then
+	COMMAND=${COMMAND}' --tty'
+fi
+
+COMMAND=${COMMAND}' \
+--volume=/var/run/docker.sock:/var/run/docker.sock \
+--volume="${XPWD:-${PWD}}:${PWD}" \
+--workdir="${PWD}" \
+--env=DEBUG=${DEBUG} \
+--env=NDEBUG=${NDEBUG}'
+
+if [ -f "${HOME}/.docker/config.json" ]; then
+	COMMAND=${COMMAND}' \
+--volume="${HOME}/.docker/config.json:/etc/docker/config.json" \
+--env=DOCKER_CONFIG=/etc/docker/config.json'
+fi
+
+COMMAND=${COMMAND}' ghcr.io/go-tk/ci:v1.3.1 make ${DEBUG:+--trace} "${@}"'
+
+eval "${COMMAND}"
